d00d2f0f1a7b4b614dc56edf7c24c0988ba8b9e5
R. A. Souza <alves.wm@gmail.com>
2020-04-04 23:29:17 -0300

    writes literals and operators

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,16 @@ fn main() {
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
     println!("Display: {}", List(vec![6,5,7,4,8]));
+
+    // Literals and Operators
+    println!("1 + 2 = {}", 1u32 + 2);
+    println!("1 - 2 = {}", 1i32 - 2);
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+    // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
 }
 
 impl fmt::Display for MyStruct {
