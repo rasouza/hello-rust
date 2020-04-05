44e46d085c845ec2c340af102d49d937736d39a7
R. A. Souza <alves.wm@gmail.com>
2020-04-04 21:37:42 -0300

    prints simple structs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+#[derive(Debug)]
+struct MyStruct(i32);
+
+#[derive(Debug)]
+struct MyDeep(MyStruct);
+
 fn main() {
 
     // String Interpolation
     println!("Hello, world!");
     println!("{0}, {1}!","Hello", "World");
-    println!("Pi is {:.prec$}", 3.145454254263452, prec=2)
+    println!("Pi is {:.prec$}", 3.145454254263452, prec=2);
 
+    // Structs
+    println!("{:?}", MyStruct(3));
+    println!("{:#?}", MyDeep(MyStruct(5)));
 }
