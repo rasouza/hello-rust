d39555600c7c4093073fab2d1dda2433cbac7c1c
R. A. Souza <alves.wm@gmail.com>
2020-04-04 21:25:46 -0300

    writes string interpolations

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    // String Interpolation
+    println!("Hello, world!");
+    println!("{0}, {1}!","Hello", "World");
+    println!("Pi is {:.prec$}", 3.145454254263452, prec=2)
+
+}
